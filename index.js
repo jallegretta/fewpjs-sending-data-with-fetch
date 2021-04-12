95fe96ec8be53ddd56987c804e8f228caaac9225
Jared <jaralleg@amazon.com>
2021-04-12 10:39:37 -0400

    building fetch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+let userData = {
+    name: "",
+    email: ""
+  };
+
+function submitData(){
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+      fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      });
+}
